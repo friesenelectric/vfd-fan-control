b9d1cae6de5dec27f063bdb82efc6336c2234730
friesenelectric <42753525+friesenelectric@users.noreply.github.com>
2018-08-28 16:48:53 -0700

    Update vfdfans.py

--- vfdfans.py
+++ vfdfans.py
@@ -4,7 +4,7 @@
 #
 # Copyright (c) 2018, Friesen Electric.
 #
-# Cvfd-fan-control is free software: you can redistribute it and/or modify it under the
+# vfd-fan-control is free software: you can redistribute it and/or modify it under the
 # terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.  See the LICENSE file
